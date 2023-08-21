af36fff8be67a8365c4280228f436fee44e99f12
isabellaccarloss <127324115+isabellaccarloss@users.noreply.github.com>
2023-08-21 14:03:39 -0400

    Update regression.py

--- 3point_analisys/regression.py
+++ 3point_analisys/regression.py
@@ -28,35 +28,16 @@ df_merge['FG2_PCT'] = df_merge['FG2M']/df_merge['FG2A']
 df_merge['OPP_FG2_PCT'] = df_merge['OPP_FG2M']/df_merge['OPP_FG2A']
 df_merge.head()
 
-# Separating in two eras: 1996 until 2011 and 2012 until 2022
-df_merge_before = df_merge[(df_merge['SEASON_1'] >= 1996) & (df_merge['SEASON_1'] <= 2011)].copy()
-df_merge_after = df_merge[(df_merge['SEASON_1'] >= 2012) & (df_merge['SEASON_1'] <= 2022)].copy()
 
-## Regression calculation for 3 pts
-# 1996-97 to 2011-12
-winpcbef_lm = smf.ols(formula = 'W_PCT ~ FG3_PCT + OPP_FG3_PCT', data=df_merge_before).fit()
-winpcbef_lm.summary()
-# 2012-13 to 2022-23
-winpc_lm = smf.ols(formula = 'W_PCT ~ FG3_PCT + OPP_FG3_PCT', data=df_merge_after).fit()
-winpc_lm.summary()
+# Separating in three eras: 1996 until 2004, 2005 until 2013, and 2014 until 2022
+df_merge_before = df_merge[(df_merge['SEASON_1'] >= 1996) & (df_merge['SEASON_1'] <= 2004)].copy()
+df_merge_mid = df_merge[(df_merge['SEASON_1'] >= 2005) & (df_merge['SEASON_1'] <= 2013)].copy()
+df_merge_after = df_merge[(df_merge['SEASON_1'] >= 2014) & (df_merge['SEASON_1'] <= 2022)].copy()
 
-# Table with regressions
-table3pt = summary_col([winpcbef_lm, winpc_lm], stars=True, float_format='%.4f',
-                     model_names=['Before 2011', 'After 2012'], info_dict={'N': lambda x: "{0:d}".format(int(x.nobs))})
-print(table3pt)
+winpcb_lm = smf.ols(formula = 'W_PCT ~ FG3_PCT + OPP_FG3_PCT + FG2_PCT + OPP_FG2_PCT', data=df_merge_before).fit()
+winpcm_lm = smf.ols(formula = 'W_PCT ~ FG3_PCT + OPP_FG3_PCT + FG2_PCT + OPP_FG2_PCT', data=df_merge_mid).fit()
+winpca_lm = smf.ols(formula = 'W_PCT ~ FG3_PCT + OPP_FG3_PCT + FG2_PCT + OPP_FG2_PCT', data=df_merge_after).fit()
 
-## Regression calculation for 2 pts
-# 1996-97 to 2011-12
-winpc2bef_lm = smf.ols(formula = 'W_PCT ~ FG2_PCT + OPP_FG2_PCT', data=df_merge_before).fit()
-winpc2bef_lm.summary()
-# 2012-13 to 2022-23
-winpc2_lm = smf.ols(formula = 'W_PCT ~ FG2_PCT + OPP_FG2_PCT', data=df_merge_after).fit()
-winpc2_lm.summary()
-
-# Table with regressions
-table2pt = summary_col([winpc2_lm, winpc2bef_lm], stars=True, float_format='%.4f',
-                     model_names=['Before 2011', 'After 2012'], info_dict={'N': lambda x: "{0:d}".format(int(x.nobs))})
-print(table2pt)
-
-## Comparing 2 pts and 3 pts influence over the seasons
-print(f"2 points",table2pt, f"\n\n\n3 points", table3pt)
+table = summary_col([winpcb_lm, winpcm_lm, winpca_lm], stars=True, float_format='%.4f',
+                     model_names=['1996-2004', '2005-2013', '2014-2022'], info_dict={'N': lambda x: "{0:d}".format(int(x.nobs))})
+print(table)
